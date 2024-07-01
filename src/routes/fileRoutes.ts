f0d4a1afaac2a53a4134dd9134ae136c782882a7
supakorn22 <6432173821@student.chula.ac.th>
2024-07-01 10:22:42 +0700

    feat: :zap: add s3 upload use multer s3

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -4,67 +4,55 @@ import { uploadFileController, downloadFile, listFiles ,statFile} from '../contr
 import path from 'path';
 import multer from 'multer';
 const router = Router();
+const { S3Client } = require('@aws-sdk/client-s3')
+import dotenv from 'dotenv';
+import multerS3 from 'multer-s3';
+// const multerS3 = require('multer-s3');
+dotenv.config();
 
 
 
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
+
+
+// Configure AWS S3 client
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
+});
+
+
+// Configure multer to use S3
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'test-s3-pooh',
+    key:  function(req: any, file: { originalname: any; }, cb: (arg0: null, arg1: any) => void) {
+          cb(null, file.originalname);
+    }
+  })
+});
 
 
 
+// // Initialize upload middleware and add file size limit
 
+router.post('/upload', upload.single('file'), (req, res) => {
+  res.send({
+    message: 'File uploaded successfully!',
+    fileUrl: req.file
+  });
+});
 
-
-// const upload = multer({
-//   storage: multer.memoryStorage(),
-//   limits: { fileSize: 100*1024 * 1024 },// 1MB
-// });
 // router.post('/upload',
 //   upload.single('file'),
 //   uploadFileController
 // );
 
-
-// Configure storage engine and filename
-const storage = multer.diskStorage({
-  destination: './uploads/',
-  filename: function(req, file, cb) {
-    // cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    cb(null, file.originalname);
-
-  }
-});
-
-// Initialize upload middleware with file size limit and file type validation
-const upload = multer({
-  storage: storage,
-  limits: { fileSize: 1 * 1024 * 1024 }, // 1MB
-  fileFilter: function(req, file, cb) {
-    // Allowed file types
-    const fileTypes = /jpeg|jpg|png|gif|rar/;
-    const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = fileTypes.test(file.mimetype);
-
-    if (mimetype && extname) {
-      return cb(null, true);
-    } else {
-      cb(new Error('worn file type, only jpeg, jpg, png, gif, rar files are allowed'));
-    }
-  }
-});
-
-
-
-// Initialize upload middleware and add file size limit
-
-
-router.post('/upload',
-  upload.single('file'),
-  uploadFileController
-);
-
-router.get('/download/:bucketName/:filePath(*)', downloadFile);
-router.get('/list/:bucketName', listFiles);
-router.get('/stat/:bucketName/:filePath(*)',statFile );
+// router.get('/download/:bucketName/:filePath(*)', downloadFile);
+// router.get('/list/:bucketName', listFiles);
+// router.get('/stat/:bucketName/:filePath(*)',statFile );
 
 export default router;
